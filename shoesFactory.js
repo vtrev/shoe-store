24d21a9af8b12705ed2de379f492d12b072bfafe
vtrev <baloyi.trev@gmail.com>
2018-10-03 23:18:59 +0200

    Added filter by shoe size brand with new tables.Code not tested,can't get postgresql to run on arch

--- shoesFactory.js
+++ shoesFactory.js
@@ -1,18 +1,17 @@
 module.exports = function ShoeServices(pool) {
-
     let getIds = async function (specs) {
         let res = {};
         try {
             if (specs.brand) {
-                const sql = 'SELECT id FROM brands WHERE brand=$1';
-                const params = [specs.brand];
-                const result = await pool.query(sql, params);
+                let sql = 'SELECT id FROM brands WHERE brand=$1';
+                let params = [specs.brand];
+                let result = await pool.query(sql, params);
                 res.brandId = result.rows[0].id;
             }
             if (specs.size) {
-                const sql = 'SELECT id FROM sizes WHERE size=$1';
-                const params = [specs.size];
-                const result = await pool.query(sql, params);
+                let sql = 'SELECT id FROM sizes WHERE size=$1';
+                let params = [specs.size];
+                let result = await pool.query(sql, params);
                 res.sizeId = result.rows[0].id;
             }
         } finally {
@@ -21,7 +20,7 @@ module.exports = function ShoeServices(pool) {
     }
 
     let getAll = async function (specs) {
-        const sql = 'SELECT qty,price,brand,color,img_link,size from shoes join sizes on size_id=sizes.id join brands on shoes.brand_id=brands.id join colors on shoes.color_id=colors.id join images on shoes.image_id=images.id'
+        let sql = 'SELECT qty,price,brand,color,img_link,size from shoes join sizes on size_id=sizes.id join brands on shoes.brand_id=brands.id join colors on shoes.color_id=colors.id join images on shoes.image_id=images.id'
         let result = await pool.query(sql);
         return result.rows
     }
@@ -40,7 +39,8 @@ module.exports = function ShoeServices(pool) {
         return result.rows
     }
     let getBrandSize = async function (specs) {
-        let sql = 'SELECT * FROM shoes WHERE brand=$1 AND size=$2';
+        let sizeBrandIds = await getIds(specs);
+        let sql = 'SELECT qty,price,brand,color,img_link,size from shoes join sizes on size_id=sizes.id join brands on shoes.brand_id=brands.id join colors on shoes.color_id=colors.id join images on shoes.image_id=images.id WHERE brand_id=$1 AND size_id=$1 ';
         let params = [specs.brand, specs.size];
         let result = await pool.query(sql, params)
         return result.rows
