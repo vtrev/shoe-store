5506a774adb3e2e2fe85f2373e78a3e872cccd2e
vtrev <baloyi.trev@gmail.com>
2018-10-09 22:53:26 +0200

    cart adding and checking out fine, the items in cart are displaying as expected, checkout does not seem to delete the qty from cart

--- shoesFactory.js
+++ shoesFactory.js
@@ -131,7 +131,11 @@ module.exports = function ShoeServices(pool) {
             for (let i = 0; i < shoesInCart.length; i++) {
                 let sql = `SELECT shoes.id,qty,price,brand,color,img_link,size from shoes join sizes on size_id=sizes.id join brands on shoes.brand_id=brands.id join colors on shoes.color_id=colors.id join images on shoes.image_id=images.id WHERE shoes.id=${shoesInCart[i].shoe_id}`;
                 let cartShoes = await pool.query(sql);
-                cart.push(cartShoes.rows[0]);
+                let shoeItem = cartShoes.rows[0];
+                let cartQtyQuery = await pool.query(`SELECT qty FROM cart WHERE shoe_id=${shoesInCart[i].shoe_id}`)
+                let cartQty = cartQtyQuery.rows[0].qty;
+                shoeItem.qty = cartQty;
+                cart.push(shoeItem);
             }
         } finally {
             return cart
